ad3471bafed885068a22c6e17d6f126605a3b6c9
ligerInfotech2023 <test.ligerinfotech@gmail.com>
2023-10-28 10:26:42 +0530

    fix fetchToken function issue

--- src/controller/caseStatus.js
+++ src/controller/caseStatus.js
@@ -11,7 +11,7 @@ const sharedAgent = new Agent({
             secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT
         }
     })
-const fetchToken = async (req,res) => {
+const fetchToken = async () => {
   const tokenValidityDuration = 30 * 60 * 1000; // 30 minutes in milliseconds
   try {
     const currentTimestamp = Date.now();
